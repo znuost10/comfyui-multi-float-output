f2c164114fce317185490e905c4e8f51003871c7
znuost10 <101150608+znuost10@users.noreply.github.com>
2025-09-22 10:42:23 -0600

    Update multi_float_output.py

--- multi_float_output.py
+++ multi_float_output.py
@@ -5,18 +5,20 @@ class MultiFloatInputNode:
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "label1": ("STRING", {"default": "Value1", "multiline": False}),
                 "value1": ("FLOAT", {"default": 0.2, "min": -10.0, "max": 10.0, "step": 0.01}),
-                "label2": ("STRING", {"default": "Value2", "multiline": False}),
                 "value2": ("FLOAT", {"default": 0.2, "min": -10.0, "max": 10.0, "step": 0.01}),
-                "label3": ("STRING", {"default": "Value3", "multiline": False}),
                 "value3": ("FLOAT", {"default": 0.2, "min": -10.0, "max": 10.0, "step": 0.01}),
-                "label4": ("STRING", {"default": "Value4", "multiline": False}),
                 "value4": ("FLOAT", {"default": 0.2, "min": -10.0, "max": 10.0, "step": 0.01}),
+                "value5": ("FLOAT", {"default": 0.2, "min": -10.0, "max": 10.0, "step": 0.01}),
+                "value6": ("FLOAT", {"default": 0.2, "min": -10.0, "max": 10.0, "step": 0.01}),
+                "value7": ("FLOAT", {"default": 0.2, "min": -10.0, "max": 10.0, "step": 0.01}),
+                "value8": ("FLOAT", {"default": 0.2, "min": -10.0, "max": 10.0, "step": 0.01}),
+                "value9": ("FLOAT", {"default": 0.2, "min": -10.0, "max": 10.0, "step": 0.01}),
+                "value10": ("FLOAT", {"default": 0.2, "min": -10.0, "max": 10.0, "step": 0.01}),
             }
         }
 
-    RETURN_TYPES = ("FLOAT", "FLOAT", "FLOAT", "FLOAT")
+    RETURN_TYPES = ("FLOAT", "FLOAT", "FLOAT", "FLOAT", "FLOAT", "FLOAT", "FLOAT", "FLOAT", "FLOAT", "FLOAT")
     FUNCTION = "output_floats"
     CATEGORY = "utils"
 
@@ -24,11 +26,17 @@ class MultiFloatInputNode:
     def IS_CHANGED(cls, **kwargs):
         return float("NaN")
 
-    def output_floats(self, value1, value2, value3, value4):
+    def output_floats(self, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10):
         return (torch.tensor([value1], dtype=torch.float32),
                 torch.tensor([value2], dtype=torch.float32),
                 torch.tensor([value3], dtype=torch.float32),
-                torch.tensor([value4], dtype=torch.float32))
+                torch.tensor([value4], dtype=torch.float32),
+                torch.tensor([value5], dtype=torch.float32),
+                torch.tensor([value6], dtype=torch.float32),
+                torch.tensor([value7], dtype=torch.float32),
+                torch.tensor([value8], dtype=torch.float32),
+                torch.tensor([value9], dtype=torch.float32),
+                torch.tensor([value10], dtype=torch.float32))
 
 NODE_CLASS_MAPPINGS = {
     "MultiFloatInputNode": MultiFloatInputNode
